21327b51572f1ccba2be14cb272356ef20ed85cf
chenen3 <enengege@gmail.com>
2023-07-23 15:04:40 +0800

    yeager 1.31.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/yeager.rb
+++ Formula/yeager.rb
@@ -1,9 +1,9 @@
 class Yeager < Formula
   desc "A proxy aims to bypass network restriction"
   homepage "https://github.com/chenen3/yeager"
-  url "https://github.com/chenen3/yeager/releases/download/v1.30.5/yeager-macos-amd64.tar.gz"
-  version "1.30.5"
-  sha256 "8b75c16c9de1cf57de6196fcb5c290bd18cdfce64e04890bf8af11fa425503b5"
+  url "https://github.com/chenen3/yeager/releases/download/v1.31.0/yeager-macos-amd64.tar.gz"
+  version "1.31.0"
+  sha256 "7db8978751a642c9ba26f746d262b80619e50595ebc0d29c6994e5f25b0e6539"
 
   def install
     bin.install "yeager"
